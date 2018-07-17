e8282187c035e58f29e97ce486dda59d252cb66b
jorgeirun <jorgemiv2@gmail.com>
2018-07-17 12:16:13 -0400

    Fix DS_Store

--- filesProcessor.py
+++ filesProcessor.py
@@ -21,20 +21,21 @@ def process(*args):
 		filesInFolder = os.listdir(currentDir+'/'+productName)
 		# Sort array by filename and save in 'lists' collection using product name as key
 		lists[productName] = sorted(filesInFolder, key = str.lower)
-	#
+	print(lists)
 	for item1, item2 in zip(lists["MOD35_L2"], lists["MOD03"]):
 		# process CloudMask and GeoLocation files in order to get GeoTiff images
 		print(item1, " - ", item2)
-		# create TIFF file name
-		tmptiffFileName = item1.split(".")
-		tiffFileName = tmptiffFileName[1]+"."+tmptiffFileName[2]
-		# create path to save tiff files
-		if not os.path.exists("tiffFiles"):
-			os.makedirs("tiffFiles")
-		tiffFilePath = os.getcwd()+"/tiffFiles"
-		print(tiffFilePath)
-		command = "MRTSwath/bin/swath2grid -if=MOD35_L2/"+item1+" -of="+tiffFilePath+"/"+tiffFileName+".tif -gf=MOD03/"+item2+" -pf=defaultparametersfile.prm"
-		# command = "MRTSwath/bin/swath2grid -if=MOD35_L2/"+item1+" -of="+tiffFilePath+"/"+tiffFileName+".tif -gf=MOD03/"+item2+" -pf=defaultparametersfile1.prm"
-		print('===== Completed =====')
-		completed = subprocess.run(command, shell=True)
-		print('returncode:', completed)
+		if item1[0] != ".": # this is to avoid using ".DS_Store" hidden file created in macosx
+			# create TIFF file name
+			tmptiffFileName = item1.split(".")
+			tiffFileName = tmptiffFileName[1]+"."+tmptiffFileName[2]
+			# create path to save tiff files
+			if not os.path.exists("tiffFiles"):
+				os.makedirs("tiffFiles")
+			tiffFilePath = os.getcwd()+"/tiffFiles"
+			print(tiffFilePath)
+			command = "MRTSwath/bin/swath2grid -if=MOD35_L2/"+item1+" -of="+tiffFilePath+"/"+tiffFileName+".tif -gf=MOD03/"+item2+" -pf=defaultparametersfile.prm"
+			# command = "MRTSwath/bin/swath2grid -if=MOD35_L2/"+item1+" -of="+tiffFilePath+"/"+tiffFileName+".tif -gf=MOD03/"+item2+" -pf=defaultparametersfile1.prm"
+			print('===== Completed =====')
+			completed = subprocess.run(command, shell=True)
+			print('returncode:', completed)
