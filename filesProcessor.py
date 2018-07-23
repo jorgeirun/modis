57679e156a61fdc3cc2fcaebaaba3407c7867da4
jorgeirun <jorgemiv2@gmail.com>
2018-07-23 17:00:52 -0400

    added AWUA filed

--- filesProcessor.py
+++ filesProcessor.py
@@ -22,6 +22,7 @@ def process(*args):
 		# Sort array by filename and save in 'lists' collection using product name as key
 		lists[productName] = sorted(filesInFolder, key = str.lower)
 	print(lists)
+	# TERRA files
 	for item1, item2 in zip(lists["MOD35_L2"], lists["MOD03"]):
 		# process CloudMask and GeoLocation files in order to get GeoTiff images
 		print(item1, " - ", item2)
@@ -35,7 +36,24 @@ def process(*args):
 			tiffFilePath = os.getcwd()+"/tiffFiles"
 			print(tiffFilePath)
 			command = "MRTSwath/bin/swath2grid -if=MOD35_L2/"+item1+" -of="+tiffFilePath+"/"+tiffFileName+".tif -gf=MOD03/"+item2+" -pf=defaultparametersfile.prm"
-			# command = "MRTSwath/bin/swath2grid -if=MOD35_L2/"+item1+" -of="+tiffFilePath+"/"+tiffFileName+".tif -gf=MOD03/"+item2+" -pf=defaultparametersfile1.prm"
 			print('===== Completed =====')
 			completed = subprocess.run(command, shell=True)
 			print('returncode:', completed)
+
+	# AQUA files
+	# for item3, item4 in zip(lists["MYD35_L2"], lists["MYD03"]):
+	# 	# process CloudMask and GeoLocation files in order to get GeoTiff images
+	# 	print(item3, " - ", item4)
+	# 	if item3[0] != ".": # this is to avoid using ".DS_Store" hidden file created in macosx
+	# 		# create TIFF file name
+	# 		tmptiffFileName = item3.split(".")
+	# 		tiffFileName = tmptiffFileName[1]+"."+tmptiffFileName[2]
+	# 		# create path to save tiff files
+	# 		if not os.path.exists("tiffFiles"):
+	# 			os.makedirs("tiffFiles")
+	# 		tiffFilePath = os.getcwd()+"/tiffFiles"
+	# 		print(tiffFilePath)
+	# 		command = "MRTSwath/bin/swath2grid -if=MYD35_L2/"+item3+" -of="+tiffFilePath+"/"+tiffFileName+".tif -gf=MYD03/"+item4+" -pf=defaultparametersfile.prm"
+	# 		print('===== Completed =====')
+	# 		completed = subprocess.run(command, shell=True)
+	# 		print('returncode:', completed)
--- main.py
+++ main.py
@@ -4,23 +4,19 @@ import modisProductDownloader
 import filesProcessor
 import os
 
-
 #
 # TEST Section
 #
-
 # item1 = "MOD35_L2.A2017001.1355.061.2017312070506.hdf"
 # item2 = "MOD03.A2017001.1355.061.2017312061337.hdf"
-
 # item3 = item1.split(".")
-
 # print(item3[1])
 # print(item3[2])
-
 # sys.exit()
 #
 # END TEST Section
 #
+
 
 #
 # VARS
@@ -31,6 +27,7 @@ coordsWest 	= "-65"
 coordsNorth = "-18"
 coordsEast 	= "-53"
 coordsSouth = "-29"
+collection	= "61" 
 
 print("===================================")
 print("===== Step 1: Download Files ======")
@@ -39,11 +36,11 @@ print("===================================")
 # Download files from MODIS repository
 # REFERENCE: modisProductDownloader.downloadFiles(productCode, collection, startDate, endDate, coordsWest, coordsNorth, coordsEast, coordsSouth)
 #
-dwnld_MOD35_L2 	= 	modisProductDownloader.downloadFiles("MOD35_L2", "61", startDate, endDate, coordsWest, coordsNorth, coordsEast, coordsSouth)
-dwnld_MOD03 	= 	modisProductDownloader.downloadFiles("MOD03", "61", startDate, endDate, coordsWest, coordsNorth, coordsEast, coordsSouth)
+dwnld_MOD35_L2 	= 	modisProductDownloader.downloadFiles("MOD35_L2", collection, startDate, endDate, coordsWest, coordsNorth, coordsEast, coordsSouth)
+dwnld_MOD03 	= 	modisProductDownloader.downloadFiles("MOD03", collection, startDate, endDate, coordsWest, coordsNorth, coordsEast, coordsSouth)
 
-# dwnld_MYD35_L2 	= 	modisProductDownloader.downloadFiles("MYD35_L2", "61", startDate, endDate, coordsWest, coordsNorth, coordsEast, coordsSouth)
-# dwnld_MYD03 	= 	modisProductDownloader.downloadFiles("MYD03", "61", startDate, endDate, coordsWest, coordsNorth, coordsEast, coordsSouth)
+# dwnld_MYD35_L2 	= 	modisProductDownloader.downloadFiles("MYD35_L2", collection, startDate, endDate, coordsWest, coordsNorth, coordsEast, coordsSouth)
+# dwnld_MYD03 	= 	modisProductDownloader.downloadFiles("MYD03", collection, startDate, endDate, coordsWest, coordsNorth, coordsEast, coordsSouth)
 
 print("===================================")
 print("======== Step 1 Completed =========")
@@ -56,6 +53,7 @@ print("===================================")
 # Process downloaded files
 # 
 processFiles = filesProcessor.process("MOD35_L2", "MOD03")
+# processFiles = filesProcessor.process("MYD35_L2", "MYD03")
 
 print("===================================")
 print("======== Step 2 Completed =========")
