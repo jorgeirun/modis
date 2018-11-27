9079fd0611360e6779fef4b3c6035ac5f541c485
Jorge <jorgeirun@me.com>
2018-11-26 21:45:38 -0300

    working with new table names modis_data_01 and modis_data01

--- database.py
+++ database.py
@@ -49,43 +49,4 @@ def createModisTable():
   except (Exception, psycopg2.DatabaseError) as error :
       print ("Message:", error)
 
-def createHeaderTable():
-  # Create header table for pixel details
-  try:
-    tableConnection = psycopg2.connect(user = user, password = password, host = "127.0.0.1", port = "5432", dbname = dbname)
-    tableConnection.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
-    cursor = tableConnection.cursor()
-    create_table_query = '''
-    	CREATE TABLE modis_data_pixels
-            (ID                	BIGSERIAL     PRIMARY KEY     NOT NULL,
-            FILENAME           	TEXT    NOT NULL,
-            LATITUDE           	TEXT    NOT NULL,
-            LONGITUDE           TEXT    NOT NULL); 
-            '''
-    cursor.execute(create_table_query)
-    tableConnection.commit()
-    print("Table modis_data_pixels created successfully.")
-  except (Exception, psycopg2.DatabaseError) as error :
-      print ("Message:", error)
-
-def createDetailsTable():
-	# Create header table for pixel details
-  try:
-    tableConnection = psycopg2.connect(user = user, password = password, host = "127.0.0.1", port = "5432", dbname = dbname)
-    tableConnection.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
-    cursor = tableConnection.cursor()
-    create_table_query = '''
-    	CREATE TABLE modis_data_pixel_details
-            (ID                		BIGSERIAL     PRIMARY KEY     NOT NULL,
-            PIXEL_ID 				TEXT    NOT NULL,
-            PIXELVALUEDATE			TEXT    NOT NULL,
-            PIXELVALUE          	TEXT    NOT NULL, 
-            PIXELBINARYVALUE        TEXT    NOT NULL, 
-            CMFLAG        			TEXT    NOT NULL,
-            CMCONFVALUE        			TEXT    NOT NULL); 
-            '''
-    cursor.execute(create_table_query)
-    tableConnection.commit()
-    print("Table modis_data_pixel_details created successfully.")
-  except (Exception, psycopg2.DatabaseError) as error :
-      print ("Message:", error)
+# TODO: create 01 to 12 tables = per month
